71fce21efcf525fb6bfa51ea02f0ae7afa1c7af4
mdd <mddnbb66@gmail.com>
2026-03-28 20:41:22 +0800

    懒加载的封装

---
+++ src/directives/lazy.js
@@ -0,0 +1,22 @@
+import { useIntersectionObserver } from "@vueuse/core";
+
+export const lazyimg = {
+  install (app){
+      app.directive("img-lazy", {
+  mounted(el, binding) {
+    const {stop } = useIntersectionObserver(el, ([entry]) => {
+      if (entry.isIntersecting) {
+        el.src = binding.value;
+        stop()
+      }
+    });
+  },
+  })
+},
+}
+
+
+
+
+
+
--- src/main.js
+++ src/main.js
@@ -4,25 +4,18 @@ import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router";
 import { useIntersectionObserver } from "@vueuse/core";
-import { shallowRef, useTemplateRef } from "vue";
+import { lazyimg } from "./directives/lazy";
 //引入全局样式
 import "@/styles/common.scss";
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
-
+app.use(lazyimg)
 
 //全局指令
-app.directive("img-lazy", {
-  mounted(el, binding) {
-    const {stop } = useIntersectionObserver(el, ([entry]) => {
-      if (entry.isIntersecting) {
-        el.src = binding.value;
-        stop()
-      }
-    });
-  },
-});
+
+
+
 
 app.mount("#app");
